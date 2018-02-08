55286433bc7961cfca0bc72149b8ae7e644cc694
Lindsay Nunez <lindsayanunez@gmail.com>
2018-02-07 20:37:51 -0500

    commented on js file the path the execution.

--- designs.js
+++ designs.js
@@ -1,1 +1,18 @@
-designs.js
+$(function(){
+
+function makeGrid(){
+
+   // clear the previous grid
+
+  // get input values for width and height
+
+  // nested loop to build grid and append it to table
+
+  // function fillS
+
+// EVENT LISTENERS
+
+  // clicking the submit button builds the grid
+ // clicking a grid square changes the background color
+
+})
